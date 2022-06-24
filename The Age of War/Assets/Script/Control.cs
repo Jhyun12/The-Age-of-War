3b1b36dd168cc91f8c19ef8fe744f505adff4965
Jhyun12 <105360218+Jhyun12@users.noreply.github.com>
2022-06-24 21:46:17 +0900

    Camera Zoom Setting

--- The Age of War/Assets/Script/Control.cs
+++ The Age of War/Assets/Script/Control.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Control : MonoBehaviour
 {
-    int damage = 10;
-    float Speed = 1.0f;
-
+    public float Speed;
+    public float HP = 100;
+    
     Animator animator;
 
     private void Start()
@@ -18,36 +18,48 @@ public class Control : MonoBehaviour
     {
         transform.Translate(Vector3.forward * Speed * Time.deltaTime);
 
+        
+
         // 애니메이터 컨트롤러에서 현재 애니메이터의 상태의 이름이“close”일 때 
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("attack1"))
         {
             // 현재 애니메이션의 진행도가 1보다 크거나 같다면 damage 변수 출력
             if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
             {
+                HP -= 10;
                 animator.Rebind();
             }
         }
-    }
 
-    public void OnTriggerStay(Collider other)
-    {
-        if(other.gameObject.tag == "Enemy")
+        if(HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+
+        RaycastHit hit;
+
+        Vector3 dir = new Vector3(transform.position.x, transform.position.z);
+
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if(Physics.Raycast(ray, out hit, 2.0f))
         {
             Speed = 0.0f;
             animator.SetBool("Attack", true);
         }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Exit")
+        else
         {
-            Speed = 1.0f;
+            Speed = 3.0f;
             animator.SetBool("Attack", false);
-
         }
-       
+
     }
+
+   
+
+
+
 
   
 }
--- The Age of War/Assets/Script/CreateMonster.cs
+++ The Age of War/Assets/Script/CreateMonster.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class CreateMonster : MonoBehaviour
 {
-   
+
+    private void Start()
+    {
+        InvokeRepeating("EnemyInstance", 0, 5);
+    }
+
     public void Create()
     {
         Instantiate(Resources.Load<GameObject>("Warrior Goblin"), new Vector3(-24, 0, 50), Quaternion.Euler(0,90,0));
                     
+    }
+
+    public void EnemyInstance()
+    {
+        int rand = Random.Range(0, 3);
+
+        if(rand == 1)
+        {
+            Instantiate(Resources.Load<GameObject>("Enemy Warrior Goblin"), new Vector3(20, 0, 50), Quaternion.Euler(0, -90, 0));
+        }
     }
 }
